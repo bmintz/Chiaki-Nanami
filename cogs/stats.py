f140adad61d953a27b2986f4b19ee35fc4e4cfaf
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2017-10-28 19:14:05 -0700

    Add a warning in on_guild_join if a guild *might* be a bot collection server

--- cogs/stats.py
+++ cogs/stats.py
@@ -47,6 +47,47 @@ class Command(_Table, table_name='commands'):
     commands_command_idx = asyncqlio.Index(command)
 
 
+# These functions are usually used for doing ratings
+# but here I'm using them to calculate if a server *might*
+# be a "bot farm". More explanation in the function itself.
+
+def _pnormaldist(n):
+    b = [1.570796288, 0.03706987906, -0.8364353589e-3,
+         -0.2250947176e-3, 0.6841218299e-5, 0.5824238515e-5,
+         -0.104527497e-5, 0.8360937017e-7, -0.3231081277e-8,
+         0.3657763036e-10, 0.6936233982e-12]
+
+    if not 0 <= n <= 1:
+        return 0
+
+    if n == 0.5:
+        return 0
+
+    w1 = n
+    if n > 0.5:
+        w1 = 1 - w1
+
+    w3 = -math.log(4.0 * w1 * (1.0 - w1))
+    iter_b = iter(b)
+    w1 = next(iter_b) + sum(v * w3 ** i for i, v in enumerate(iter_b, 1))
+
+    result = math.sqrt(w1 * w3)
+    return result if n > 0.5 else -result
+
+
+def _ci_lower_bound(pos, n, confidence):
+    if pos > n:
+        raise ValueError('number of positive ratings must be lower than the total')
+
+    if n == 0:
+        return 0
+
+    z = _pnormaldist(1-(1-confidence)/2)
+    phat = 1.0*pos/n
+    return (phat + z*z/(2*n) - z * math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
+
+
+
 class Stats(Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -204,7 +245,24 @@ class Stats(Cog):
         """Tests the error logger webhook."""
         NO
 
-    async def send_guild_stats(self, guild, colour, header):
+    # Determining if a server is a "bot collection server" is no easy task,
+    # because there are a lot of edge cases in servers where it might not be
+    # a bot farm but merely a testing server with only a few bots.
+    @staticmethod
+    def is_bot_farm(guild):
+        """Return True if the guilds is considered to be a "bot farm".
+
+        Bot farms are guilds where the bot-to-member ratio is extremely high.
+        They're essentially bot collection servers. These are very useless
+        to the bot and could even pose problems as people in those bot farms
+        like to hammer bots with a lot of commands.
+        """
+        bots = sum(m.bot for m in guild.members)
+        total = guild.member_count
+
+        return _ci_lower_bound(bots, total, 0.9) >= 0.42
+
+    async def send_guild_stats(self, guild, colour, header, *, check_bot_farm=True):
         bots = sum(m.bot for m in guild.members)
         total = guild.member_count
         online = sum(m.status is discord.Status.online for m in guild.members)
@@ -225,13 +283,17 @@ class Stats(Cog):
         if guild.me:
             e.timestamp = guild.me.joined_at
 
+        if check_bot_farm and _ci_lower_bound(bots, total, 0.9) >= 0.42:
+            e.description = '\N{WARNING SIGN} This server **might** be a bot collection server.'
+
         await self.bot.webhook.send(embed=e)
 
     async def on_guild_join(self, guild):
-        await self.send_guild_stats(guild, 0x53dda4, 'New')
+        await self.send_guild_stats(guild, 0x53dda4, 'New',)
 
     async def on_guild_remove(self, guild):
-        await self.send_guild_stats(guild, 0xdd5f53, 'Left')
+        # No need to check if Chiaki left a bot collection server lol.
+        await self.send_guild_stats(guild, 0xdd5f53, 'Left', check_bot_farm=False)
 
 
 def setup(bot):
