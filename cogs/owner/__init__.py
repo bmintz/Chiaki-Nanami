6a0adf525db53c26a71dd13473d5a2aeca43d506
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2018-04-01 18:46:02 -0700

    Cut down repetition on loading packages
    Aside from hi-jacking Chiaki.load_extension which I don't feel like
    doing, or utilizing frame-hacks, this is the best way I could think of
    to cut down on the repetition.

--- cogs/owner/__init__.py
+++ cogs/owner/__init__.py
@@ -1,23 +1,3 @@
 """**For the bot owner only!"""
-
-def _get_module_names():
-    import pathlib
-    path = pathlib.Path(__file__).parent
-    return (__name__ + '.' + m.stem for m in path.glob('*.py') if m.stem != '__init__')
-
-
-def setup(bot):
-    import discord
-    for module_name in _get_module_names():
-        try:
-            bot.load_extension(module_name)
-        except discord.ClientException:
-            pass
-        except:
-            teardown(bot)
-            raise
-
-
-def teardown(bot):
-    for module_name in _get_module_names():
-        bot.unload_extension(module_name)
+from ..utils.init import init
+setup, teardown = init(__name__, __file__)
