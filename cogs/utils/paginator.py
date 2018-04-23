11a1f1d0db08615f0bb22687a22617d74cfb40a6
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2018-04-22 18:28:07 -0700

    Remove DelimPaginator
    No other function uses this, and it can easily be made
    as a function.

--- cogs/utils/paginator.py
+++ cogs/utils/paginator.py
@@ -14,44 +14,6 @@ from .deprecated import DeprecatedCommand
 from .misc import maybe_awaitable
 
 
-class DelimPaginator(commands.Paginator):
-    def __init__(self, prefix='```', suffix='```', max_size=2000, join_delim='\n', **kwargs):
-        super().__init__(prefix, suffix, max_size)
-        self.escape_code = kwargs.get('escape_code', False)
-        self.join_delim = join_delim
-
-    def __len__(self):
-        return len(self.pages)
-
-    def __getitem__(self, x):
-        return self.pages[x]
-
-    def add_line(self, line, escape_code=False):
-        line = line.replace('`', '\u200b`') if self.escape_code else line
-        super().add_line(line)
-
-    def close_page(self):
-        """Prematurely terminate a page."""
-        self._current_page.append(self.suffix)
-        prefix, *rest, suffix = self._current_page
-        self._pages.append(f"{prefix}{self.join_delim.join(rest)}{suffix}")
-        self._current_page = [self.prefix]
-        self._count = len(self.prefix) + 1  # prefix + newline
-
-    @classmethod
-    def from_iterable(cls, iterable, **kwargs):
-        paginator = cls(**kwargs)
-        for i in iterable:
-            paginator.add_line(i)
-        return paginator
-
-    @property
-    def total_size(self):
-        return sum(map(len, self))
-
-
-# --------------------- Embed-related things ---------------------
-
 def page(emoji):
     def decorator(func):
         func.__reaction_emoji__ = emoji
