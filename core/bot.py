5c9a0dffe356513f1431fd32a7c45d1ecb18d9b3
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2018-04-29 13:08:29 -0700

    Version bump to 1.2.4

--- core/bot.py
+++ core/bot.py
@@ -145,8 +145,8 @@ VersionInfo = collections.namedtuple('VersionInfo', 'major minor micro releasele
 
 
 class Chiaki(commands.Bot):
-    __version__ = '1.2.3'
-    version_info = VersionInfo(major=1, minor=2, micro=3, releaselevel='final', serial=0)
+    __version__ = '1.2.4'
+    version_info = VersionInfo(major=1, minor=2, micro=4, releaselevel='final', serial=0)
 
     def __init__(self):
         super().__init__(command_prefix=_callable_prefix,
