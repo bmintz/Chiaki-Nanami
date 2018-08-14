ae7655a454ad887efb260bce51d2e3e0aced059d
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2018-08-14 02:50:21 -0700

    Add BadArgument and BadUnionArgument handling
    These were the hardest exception to handle, simply because it requires
    *a lot* of weird hacks.
    
    This can vastly be simplified by subclassing Command and raising a
    different exception, but it feels a little off for some reason.

--- core/errors.py
+++ core/errors.py
@@ -1,12 +1,15 @@
 import datetime
+import inspect
 import itertools
 import json
 import random
+import re
 import sys
 import traceback
 
 import discord
 from discord.ext import commands
+from more_itertools import last
 
 from cogs.utils.examples import _parameter_examples, _split_params
 from cogs.utils.formats import human_join
@@ -77,10 +80,127 @@ def missing_required_arg(ctx, error):
 
 # ----------- BadArgument -----------
 
+_reverse_quotes = {cq: oq for oq, cq in commands.view._quotes.items()}
+_clean_content = commands.clean_content(fix_channel_mentions=True, escape_markdown=True)
+
+def _get_bad_argument(ctx, param):
+    content = ctx.message.content
+    view = ctx.view
+    if param.kind == param.KEYWORD_ONLY and not ctx.command.rest_is_raw:
+        # Keyword only arguments are interpreted as "consume rest", unless
+        # rest_is_raw is True. Internally, this means that view.read_rest() is
+        # called as opposed to commands.view.quoted_word(view). This means we
+        # can just grab the whole string via view.previous.
+        return content[view.previous:], view.previous
+
+    # It's a non-consume-rest arg which means we need to figure out where
+    # the bad argument started. Believe it or not this is extremely hard as
+    # commands.view.quoted_word(view) calls view.get() repeatedly, which
+    # corrupts view.previous as now it's only the previous character rather
+    # than the whole word. What makes it worse are the quotes, as that means
+    # we can't just find the "last word" in that string.
+    #
+    # An alternative way would be to monkey patch quoted_word to set something
+    # like view.quoted_word_previous = view.index before calling the real
+    # one, but such thing would be ridiculously horrid, and would probably
+    # be more fragile, as it assumes that StringView isn't slotted and
+    # quoted_word would stay "public".
+
+    # Anything past view.index can't (or shouldn't) be checked for validity,
+    # so we can safely discard it.
+    content = ctx.message.content[:ctx.view.index]
+    bad_quote = content[-1]
+    bad_open_quote = _reverse_quotes.get(bad_quote)
+    if not bad_open_quote or content[-2:-1] in ['\\', '']:
+        # If there was no quote, or if it was escaped, then we can just
+        # chomp off the whitespace up to that point.
+        #
+        # Use rsplit instead of rpartition as the former can take any
+        # whitespace. (ext can take any sort of whitespace)
+        bad_content = content.rsplit(None, 1)[-1]
+
+        return bad_content, view.index - len(bad_content)
+
+    # We need to look for the last "quoted" word.
+    quote_pattern = rf'{bad_open_quote}(?:[^{bad_quote}\\]|\\.)*{bad_quote}'
+    last_match = last(re.finditer(quote_pattern, content))
+    # I swear if last_match is None...
+    assert last_match, f'last_match is None with content {content}'
+    return last_match[1], last_match.start()
+
+async def _format_bad_argument(ctx, param, error):
+    _, end_content_at = _get_bad_argument(ctx, param)
+    content = ctx.message.content[:end_content_at]
+
+    # The only reason why this function is async def
+    content = await _clean_content.convert(ctx, content)
+
+    # Show the rest of the args, because we're not using the original text past
+    # the bad argument we may as well show valid examples.
+    required, optional = map(iter, _split_params(ctx.command))
+    # These are iterators so consuming them in chain will also consume them outside.
+    next(itertools.dropwhile(lambda p: p != param, itertools.chain(required, optional)), None)
+    example = f'**{next(_parameter_examples([param], ctx))}**'
+
+    other_examples = ' '.join(_parameter_examples(itertools.chain(required, _random_slice(list(optional))), ctx))
+    if other_examples:
+        example = example + ' ' + other_examples
+
+    return (
+        f'{error}\n\n'
+        f'Usage: `{ctx.clean_prefix}{ctx.command.signature}`\n'
+        f'Example: {content}{example}'
+    )
+
 @_handler(commands.BadArgument)
-def _bad_argument(ctx, error):
-    # TODO: Handle this properly when discord.py isn't stupid with BadArgument
-    return ctx.send(error or error.__cause__)
+async def _bad_argument(ctx, error):
+    # This can probably be simplified if I made BadArgument raise
+    # with something else.
+    params = list(ctx.command.params.values())
+    index = min(len(ctx.args) + len(ctx.kwargs), len(params) - 1)
+    param = params[index]
+
+    error = error or error.__cause__
+    if isinstance(error.__cause__, ValueError):
+        # Capture any errors caused by int or float. This is not limited to just
+        # int and float, but also any converters that call them as well.
+        cause = str(error.__cause__)
+        if cause.startswith((
+            'invalid literal for int()',         # int
+            'could not convert string to float'  # float
+        )):
+            # This is the one of the few times we need to extract out the argument
+            # because the error is bad. Thankfully it's easy to extract here.
+            match = re.search(r": '(.*)'", cause)
+            error = f'"{match[1]}" is not a number.'
+
+    message = await _format_bad_argument(ctx, param, error)
+    await ctx.send(message)
+
+# ----------- BadUnionArgument -----------
+
+def _format_converter(converter):
+    if converter == int:
+        return 'number'
+    return converter.__name__.lower()
+
+def _format_converters(converters):
+    # Right now the only things I have are "int", "Member", and the channels.
+    # When I make typing.Union a normal thing (which might never happen because
+    # of disambiguation), this probably won't be as hacky.
+
+    if all(inspect.isclass(c) and issubclass(c, discord.abc.GuildChannel) for c in converters):
+        return 'channel'
+
+    return human_join(map(_format_converter, converters), final='or')
+
+@_handler(commands.BadUnionArgument)
+async def _bad_union_argument(ctx, error):
+    bad_argument, _ = _get_bad_argument(ctx, error.param)
+    error_message = f'"{bad_argument}" is not a {_format_converters(error.converters)}.'
+
+    message = await _format_bad_argument(ctx, error.param, error_message)
+    await ctx.send(message)
 
 # ----------- NoPrivateMessage ------------
 
